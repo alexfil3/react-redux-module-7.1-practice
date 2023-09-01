1ec85ba7b390e4c5ad71ed09f0a3f124e566d343
Alex Filimonov <filimonovalexander9997@gmail.com>
2023-09-01 22:27:49 +0300

    updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { Layout } from 'components/Layout/Layout';
+import { AppBar } from 'components/AppBar/AppBar';
+import { TaskForm } from 'components/TaskForm/TaskForm';
+import { TaskList } from 'components/TaskList/TaskList';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchTasks } from 'redux/operations';
+import { getError, getIsLoading } from 'redux/selectors';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010102',
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <TaskList />
+    </Layout>
   );
 };
---
+++ src/components/Task/Task.jsx
@@ -0,0 +1,28 @@
+import { useDispatch } from 'react-redux';
+import { deleteTask } from 'redux/operations';
+import { toggleCompleted } from 'redux/operations';
+import { MdClose } from 'react-icons/md';
+import css from './Task.module.css';
+
+export const Task = ({ task }) => {
+  const dispatch = useDispatch();
+
+  const handleToggle = () => dispatch(toggleCompleted(task));
+
+  const handleDelete = () => dispatch(deleteTask(task.id));
+
+  return (
+    <div className={css.wrapper}>
+      <input
+        type="checkbox"
+        className={css.checkbox}
+        checked={task.completed}
+        onChange={handleToggle}
+      />
+      <p className={css.text}>{task.text}</p>
+      <button className={css.btn} onClick={handleDelete}>
+        <MdClose size={24} />
+      </button>
+    </div>
+  );
+};
